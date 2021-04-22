0541531347d1cd447bf3490bf7bcebc2f5c0159a
Hayden Braxton <haydenbraxton@haydenbraxton.com>
2021-04-22 09:25:33 -0400

    add kub to init script

--- init.sh
+++ init.sh
@@ -4,6 +4,7 @@ brew install --cask slack
 brew install --cask zoom
 brew install --cask vscodium
 brew install docker
+brew install kubernetes-cli
 
 brew tap federico-terzi/espanso
 brew install espanso
