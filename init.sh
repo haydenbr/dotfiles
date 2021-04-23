5c7b5b8dcb78923a26afdf9f839986aa309dacde
Hayden Braxton <haydenbraxton@haydenbraxton.com>
2021-04-23 09:07:21 -0400

    fix nvm install

--- init.sh
+++ init.sh
@@ -5,8 +5,14 @@ brew install --cask zoom
 brew install --cask vscodium
 brew install --cask docker
 brew install kubernetes-cli
-brew install nvm
 
+# nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+# espanso
 brew tap federico-terzi/espanso
 brew install espanso
 espanso --version
