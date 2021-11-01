22f6251438319f7b26214469bb528a1285c06d59
Hayden Braxton <haydenbraxton@haydenbraxton.com>
2021-10-31 20:04:43 -0400

    add default branch name as main. other dependency updates

--- init.sh
+++ init.sh
@@ -1,21 +1,34 @@
 #! /bin/sh
 
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+brew update
+
 brew install --cask slack
 brew install --cask zoom
-brew install --cask vscodium
+brew install --cask visual-studio-code
 brew install --cask docker
 brew install kubernetes-cli
 brew install --cask dotnet-sdk
 brew install md5sha1sum
+brew install --cask firefox
+brew install --cask iterm2
+brew install wireshark
+brew install awscli
+brew install jq
+brew install openssl
+brew tap federico-terzi/espanso
+brew install espanso
+
+bnew upgrade
 
 # nvm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 # espanso
-brew tap federico-terzi/espanso
-brew install espanso
 espanso --version
 espanso register
+
--- sync.sh
+++ sync.sh
@@ -6,7 +6,6 @@ cp ~/Projects/dotfiles/.gitconfig ~/.gitconfig
 cp ~/Projects/dotfiles/git-completion.bash ~/git-completion.bash
 cp ~/Projects/dotfiles/git-prompt.sh ~/git-prompt.sh
 cp -r ~/Projects/dotfiles/espanso ~/Library/Preferences
-cp -r ~/Projects/dotfiles/VSCodium/User ~/Library/Application\ Support/VSCodium
 
 source ~/.bash_profile
 espanso restart
